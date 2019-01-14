bcf3021b653c46a36c3ab45d96977e9f74a91fe2
lzf_ccsu <854047294@qq.com>
2019-01-14 17:44:11 +0800

    2019-01-14

---
+++ practied/pratice_1.js
@@ -0,0 +1,17 @@
+
+const a = [1, 2, 3, 4, 5]
+
+Object.defineProperty(Array.prototype, 'multiply', {
+    value: function () {
+        this.push(...this.map(v => v * v));
+    }
+});
+
+Array.prototype.multiply = function () {
+    this.push(...this.map(v => v * v));
+    return this;
+}
+
+a.multiply();
+
+console.info(a);
