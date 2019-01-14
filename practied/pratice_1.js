0fc434bd78ec45ee95bfa68d333d6adc253337a4
lzf_ccsu <854047294@qq.com>
2019-01-14 18:06:21 +0800

    练习1

--- practied/pratice_1.js
+++ practied/pratice_1.js
@@ -1,4 +1,16 @@
+// 题目
+// const a = [1, 2, 3, 4, 5];
+// // Implement this
+// a.multiply();
+// console.log(a); // [1, 2, 3, 4, 5, 1, 4, 9, 16, 25]
 
+// 知识点
+
+/**
+ * 为原型添加一个方法
+ * 数据常用方法的使用
+ * 解构赋值
+ */
 const a = [1, 2, 3, 4, 5]
 
 Object.defineProperty(Array.prototype, 'multiply', {
